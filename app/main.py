873475108d44c5c5f0489955070bb5eedb347c4b
nazhiifyahya <mnazhiifys@gmail.com>
2022-06-02 08:12:06 +0700

    added most functionality

--- app/main.py
+++ app/main.py
@@ -1,16 +1,6 @@
-from re import sub
-from flask import Flask, render_template, session, redirect, url_for
-import mysql.connector
-
-def get_db_connection():
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="obe_system"
-    )
-
-    return conn
+from flask import Flask, render_template, request, session, redirect, url_for
+from db_conn import *
+from obj import *
 
 app = Flask(__name__)
 app.secret_key = 'thisIsSecret'
@@ -24,13 +14,47 @@ def home():
 
 @app.route('/login')
 def login():
-    session['user'] = 'nazhiif'
-    return redirect(url_for('home'))
+    return render_template('login.html')
+
+@app.route('/login/educator', methods = ['GET', 'POST'])
+def loginEducator():
+    if (request.method == 'GET'):
+        return render_template('loginEducator.html')
+    else:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM tenagapendidik')
+        result = cursor.fetchall()
+        conn.close()
+
+        for entry in result:
+            if (request.form['username'] == entry[2]):
+                if (request.form['password'] == entry[3]):
+                    session['user'] = entry[1]
+                    if (entry[5] == 1):
+                        return redirect(url_for('educatorSubject'))
+                    return redirect(url_for('adminCPL'))
+        return redirect(url_for('loginEducator'))
+        
+@app.route('/login/student', methods = ['GET', 'POST'])
+def loginStudent():
+    if (request.method == 'GET'):
+        return render_template('loginStudent.html')
+    else:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM mahasiswa')
+        result = cursor.fetchall()
+        conn.close()
+
+        for entry in result:
+            if (request.form['username'] == entry[2]):
+                if (request.form['password'] == entry[3]):
+                    session['user'] = entry[1]
+                    return redirect(url_for('studentSubject'))
+        return redirect(url_for('loginStudent'))
 
 @app.route('/student')
-def student():
-    return 'hello student'
-
 @app.route('/student/subject')
 def studentSubject():
     conn = get_db_connection()
@@ -46,16 +70,33 @@ def studentSubject():
             'name': entry[1]
         }
         subjects.append(subject)
-    return render_template('studentSubject.html', subject = subjects)
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM enroll WHERE NIM = "1953" AND YEAR(date) = 2022')
+    result2 = cursor.fetchall()
+    conn.close()
+
+    enrolled = []
+    for entry in result2 :
+        enrolled.append(entry[1])
+
+    return render_template('studentSubject.html', subject = subjects, enr = enrolled)
+
+@app.route('/student/<subjectCode>/enroll', methods = ['POST'])
+def studentSubjectEnroll(subjectCode):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO enroll(NIM, subjectCode) VALUES ("1953", "%s")' % (subjectCode,))
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for('studentSubject'))
+
 
 @app.route('/student/subject/<subjectCode>')
 def studentSubjectDetails(subjectCode):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM subject WHERE subjectCode = %s' % (subjectCode,))
-    result = cursor.fetchall()
-    conn.close()
-    return subjectCode
+    return render_template('studentSubjectDetails.html', subject = Subject(subjectCode))
 
 @app.route('/student/enrolled')
 def studentEnrolled():
@@ -67,8 +108,240 @@ def studentEnrolledDetails(subjectCode):
 
 @app.route('/student/grades')
 def studentGrades():
-    return 'grade'
+    enroll = Enroll('1953', '2022')
+    subjectGrade = []
+    for e in enroll.enrolled:
+        sg = SubjectGrade(e.code, '1953', '2022', '2022')
+        subjectGrade.append(sg)
+    return render_template('studentGrades.html', subjectGrade = subjectGrade)
 
 @app.route('/student/grades/<subjectCode>')
 def studentGradesDetails(subjectCode):
-    return subjectCode
+    return render_template('studentGradesDetails.html', subjectGrade = SubjectGrade(subjectCode, '1953', '2022', '2022'))
+
+
+@app.route('/admin')
+@app.route('/admin/CPL')
+def adminCPL():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT cplCode FROM cpl')
+    result = cursor.fetchall()
+    conn.close()
+
+    cpls = []
+    for entry in result:
+        cpl = Cpl(entry[0])
+        cpls.append(cpl)
+    return render_template('adminCpl.html', cpl = cpls)
+
+@app.route('/admin/cpl/add', methods=['GET', 'POST'])
+def adminAddCpl():
+    if request.method == 'GET':
+        return render_template('adminCplAdd.html')
+    else:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO cpl (cplCode, cplDesc) VALUES ("%s", "%s")' % (request.form['cplCode'], request.form['cplDesc']))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('adminCPL'))
+
+@app.route('/admin/subject')
+def adminSubject():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT subjectCode FROM subject')
+    result = cursor.fetchall()
+    conn.close()
+
+    subjectList = []
+    for entry in result:
+        subject = Subject(entry[0])
+        subjectList.append(subject)
+    
+    return render_template('adminSubject.html', list = subjectList)
+
+@app.route('/admin/subject/add', methods=['GET', 'POST'])
+def adminSubjectAdd():
+    if request.method == 'GET':
+        return render_template('adminSubjectAdd.html')
+    else:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO subject (subjectCode, subjectName) VALUES ("%s", "%s")' % (request.form['subjectCode'], request.form['subjectName']))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('adminSubject'))
+
+@app.route('/admin/assign/cpl')
+def adminAssignCpl():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT cplCode FROM cpl')
+    result = cursor.fetchall()
+    conn.close()
+
+    cpls = []
+    for entry in result:
+        cpl = Cpl(entry[0])
+        cpls.append(cpl)
+    return render_template('adminAssignCpl.html', cpl = cpls)
+
+@app.route('/admin/assign/<cplCode>/subject', methods=['GET', 'POST'])
+def adminAssignCplSubject(cplCode):
+    if request.method == 'GET':
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT subjectCode FROM subject')
+        result = cursor.fetchall()
+        conn.close()
+
+        subjectList = []
+        for entry in result:
+            subject = Subject(entry[0])
+            subjectList.append(subject)
+
+        return render_template('adminAssignCplSubject.html', list = subjectList, cplCode = cplCode)
+    else:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('UPDATE cpl SET subjectCode = "%s" WHERE cplCode = "%s"' % (request.form['subjectCode'], cplCode))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('adminAssignCpl'))
+
+@app.route('/admin/assign/subject')
+def adminAssignSubject():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT subjectCode FROM subject')
+    result = cursor.fetchall()
+    conn.close()
+
+    subjectList = []
+    for entry in result:
+        subject = Subject(entry[0])
+        subjectList.append(subject)
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM course')
+    result2 = cursor.fetchall()
+    conn.close()
+
+    courseList = []
+    for entry in result2:
+        courseList.append(entry)
+
+    return render_template('adminAssignSubject.html', list = subjectList, courseList = courseList)
+
+@app.route('/admin/assign/<subjectCode>/educator', methods=['GET', 'POST'])
+def adminAssignSubjectEducator(subjectCode):
+    if request.method == 'GET':
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM tenagapendidik WHERE isDosen = 1')
+        result = cursor.fetchall()
+        conn.close()
+
+        educatorList = []
+        for entry in result:
+            educatorList.append(entry)
+
+        return render_template('adminAssignSubjectEducator.html', list = educatorList, subjectCode = subjectCode)
+    else:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO course (NIP, subjectCode) VALUES ("%s", "%s")' % (request.form['nip'], subjectCode))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('adminAssignSubject'))
+
+@app.route('/educator')
+@app.route('/educator/subject')
+def educatorSubject():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT subject.subjectCode, subject.subjectName FROM subject INNER JOIN course ON subject.subjectCode=course.subjectCode WHERE course.NIP=2022')
+    result = cursor.fetchall()
+    conn.close()
+
+    subjectList = []
+    for entry in result:
+        subject = Subject(entry[0])
+        subjectList.append(subject)
+    
+    return render_template('educatorSubject.html', list = subjectList)
+
+@app.route('/educator/<subjectCode>/cpmk')
+def educatorSubjectCpmk(subjectCode):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM cpmk WHERE subjectCode = "%s"' % (subjectCode))
+    result = cursor.fetchall()
+    conn.close()
+
+    cpmk = []
+    for entry in result:
+        cpmk.append(entry)
+    
+    return render_template('educatorSubjectCpmk.html', cpmk = cpmk, subjectCode = subjectCode)
+
+@app.route('/educator/<subjectCode>/cpmk/add', methods=['GET', 'POST'])
+def educatorSubjectCpmkAdd(subjectCode):
+    if (request.method == 'GET'):
+        return render_template('educatorSubjectCpmkAdd.html', subjectCode = subjectCode)
+    else:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO cpmk VALUES ("%s", "%s", "%s", %s)' % (request.form['cpmkCode'], subjectCode, request.form['cpmkDesc'], request.form['gradePercent']))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('educatorSubjectCpmk', subjectCode = subjectCode))
+
+@app.route('/educator/grade')
+def educatorGrade():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT subject.subjectCode, subject.subjectName FROM subject INNER JOIN course ON subject.subjectCode=course.subjectCode WHERE course.NIP=2022')
+    result = cursor.fetchall()
+    conn.close()
+
+    subjectList = []
+    for entry in result:
+        subject = Subject(entry[0])
+        subjectList.append(subject)
+    
+    return render_template('educatorGrade.html', list = subjectList)
+
+@app.route('/educator/<subjectCode>/student')
+def educatorGradeStudent(subjectCode):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM enroll WHERE subjectCode = "%s"' % (subjectCode,))
+    result = cursor.fetchall()
+    conn.close()
+
+    students = []
+    for entry in result:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT nama FROM mahasiswa WHERE NIM = "%s"' % (entry[0]))
+        name = cursor.fetchone()[0]
+        conn.close()
+
+        student = [entry[0], name]
+        students.append(student) 
+    
+    return render_template('educatorGradeStudents.html', students = students, subjectCode = subjectCode)
+
+@app.route('/logout')
+def logout():
+    session['user'] = ''
+    return redirect(url_for('login'))
---
+++ app/obj.py
@@ -0,0 +1,276 @@
+from flask import g
+from db_conn import *
+
+class AssessmentIndicator:
+    def __init__(self, subjectCode, cpmkCode, subCpmkCode, code, name, gradePercent):
+        self.subjectCode = subjectCode
+        self.cpmkCode = cpmkCode
+        self.subCpmkCode = subCpmkCode
+        self.code = code
+        self.name = name
+        self.gradePercent = gradePercent
+    
+class SubCpmk:
+    def __init__(self, subjectCode, cpmkCode, code, name, gradePercent):
+        self.subjectCode = subjectCode
+        self.cpmkCode = cpmkCode
+        self.code = code
+        self.name = name
+        self.gradePercent = gradePercent
+        self.assessmentIndicator = []
+        self.setAssessmentIndicator()
+    
+    def setAssessmentIndicator(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM indikatorpenilaian WHERE subjectCode = "%s" AND cpmkCode = "%s" AND subCpmkCode = "%s"' % (self.subjectCode, self.cpmkCode, self.code))
+        result = cursor.fetchall()
+        conn.close()
+
+        for entry in result:
+            assessInd = AssessmentIndicator(self.subjectCode, self.cpmkCode, self.code, entry[0], entry[4], entry[5])
+            self.assessmentIndicator.append(assessInd)
+
+class Cpmk:
+    def __init__(self, subjectCode, code, name, gradePercent):
+        self.subjectCode = subjectCode
+        self.code = code
+        self.name = name
+        self.gradePercent = gradePercent
+        self.subCpmk = []
+        self.setSubCpmk()
+    
+    def setSubCpmk(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM subcpmk WHERE subjectCode = "%s" AND cpmkCode = "%s"' % (self.subjectCode, self.code))
+        result = cursor.fetchall()
+        conn.close()
+
+        for entry in result:
+            subCpmk = SubCpmk(self.subjectCode, self.code, entry[0], entry[3], entry[4])
+            self.subCpmk.append(subCpmk)
+
+class Subject:
+    def __init__(self, code):
+        self.code = code
+        self.name = ''
+        self.cpmk = []
+        self.setName()
+        self.setCpmk()
+
+    def setName(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT subjectName FROM subject WHERE subjectCode = "%s"' % (self.code,))
+        self.name = cursor.fetchone()[0]
+        conn.close()
+
+    def setCpmk(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM cpmk WHERE subjectCode = "%s"' % (self.code,))
+        result = cursor.fetchall()
+        conn.close()
+
+        for entry in result:
+            cpmk = Cpmk(self.code, entry[0], entry[2], entry[3])
+            self.cpmk.append(cpmk)
+
+class AssessmentIndicatorGrade:
+    def __init__(self, subjectCode, cpmkCode, subCpmkCode, code, nim, nip, year):
+        self.subjectCode = subjectCode
+        self.cpmkCode = cpmkCode
+        self.subCpmkCode = subCpmkCode
+        self.code = code
+        self.nim = nim
+        self.nip = nip
+        self.subCpmkGradePercent = 0
+        self.year = year
+        self.grade = 0
+
+        self.setGradePercent()
+        self.setGrade()
+    
+    def setGradePercent(self):
+        subject = Subject(self.subjectCode)
+        for c in subject.cpmk:
+            if (c.code == self.cpmkCode):
+                for s in c.subCpmk:
+                    if (s.code == self.subCpmkCode): 
+                        for i in s.assessmentIndicator:
+                            if (i.code == self.code):
+                                self.subCpmkGradePercent = i.gradePercent
+
+
+    def setGrade(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT grade FROM indikatorpenilaiangrade WHERE subjectCode = "%s" AND cpmkCode = "%s" AND subCpmkCode = "%s" AND indikatorPenilaianCode = "%s" AND NIM = "%s" AND NIP = "%s" AND YEAR(date) = %s' % (self.subjectCode, self.cpmkCode, self.subCpmkCode, self.code, self.nim, self.nip, self.year))
+        self.grade = cursor.fetchone()[0]
+        conn.close()
+
+class SubCpmkGrade:
+    def __init__(self, subjectCode, cpmkCode, code, nim, nip, year):
+        self.subjectCode = subjectCode
+        self.cpmkCode = cpmkCode
+        self.code = code
+        self.nim = nim
+        self.nip = nip
+        self.cpmkGradePercent = 0
+        self.year = year
+        self.grade = 0
+
+        self.setGradePercent()
+        self.setGrade()
+    
+    def setGradePercent(self):
+        subject = Subject(self.subjectCode)
+        for c in subject.cpmk:
+            if (c.code == self.cpmkCode):
+                for s in c.subCpmk:
+                    if (s.code == self.code):
+                        self.cpmkGradePercent = s.gradePercent
+                            
+
+    def setGrade(self):
+        assIndGrade = []
+        subject = Subject(self.subjectCode)
+        for c in subject.cpmk:
+            if (c.code == self.cpmkCode):
+                for s in c.subCpmk:
+                    if (s.code == self.code):
+                        for i in s.assessmentIndicator:
+                            indGrade = AssessmentIndicatorGrade(subject.code, c.code, s.code, i.code, self.nim, self.nip, self.year)
+                            gradeAndPercent = [indGrade.subCpmkGradePercent, indGrade.grade]
+                            assIndGrade.append(gradeAndPercent)
+        
+        for g in assIndGrade:
+            self.grade += g[1]*g[0]/100
+    
+    def inputGrade(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO subcpmkgrade (subCpmkCode, cpmkCode, subjectCode, NIM, NIP, grade) VALUES ("%s", "%s", "%s", "%s", "%s", %s)' % (self.code, self.cpmkCode, self.subjectCode, self.nim, self.nip, self.grade))
+        conn.commit()
+        conn.close()
+    
+class CpmkGrade:
+    def __init__(self, subjectCode, code, nim, nip, year):
+        self.subjectCode = subjectCode
+        self.subject = Subject(self.subjectCode)
+        self.code = code
+        self.nim = nim
+        self.nip = nip
+        self.year = year
+        self.subjectGradePercent = 0
+        self.subCpmkGrades = []
+        self.grade = 0
+
+        self.setSubCpmkGrades()
+        self.setGradePercent()
+        self.setGrade()
+
+    def setSubCpmkGrades(self):
+        subject = Subject(self.subjectCode)
+        for c in subject.cpmk:
+            if (c.code == self.code):
+                for s in c.subCpmk:
+                    subCpmk = SubCpmkGrade(subject.code, c.code, s.code, self.nim, self.nip, self.year)
+                    self.subCpmkGrades.append(subCpmk)
+    
+    def setGradePercent(self):
+        subject = Subject(self.subjectCode)
+        for c in subject.cpmk:
+            if (c.code == self.code):
+                self.subjectGradePercent = c.gradePercent
+
+    def setGrade(self):
+        for s in self.subCpmkGrades:
+            self.grade += s.grade*s.cpmkGradePercent/100
+    
+class FinalExamGrade:
+    def __init__(self, subjectCode, nim, nip, year):
+        self.subjectCode = subjectCode
+        self.nim = nim
+        self.nip = nip
+        self.year = year
+        self.grade = 0
+        
+    
+    def setGrade(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT grade FROM finalexamgrade WHERE subjectCode = "%s" AND NIM = "%s" AND NIP = "%s" AND YEAR(date) = %s' % (self.subjectCode, self.nim, self.nip, self.year))
+        self.grade = cursor.fetchone()[0]
+        conn.close()
+
+class SubjectGrade:
+    def __init__(self, subjectCode, nim, nip, year):
+        self.subjectCode = subjectCode
+        self.subject = Subject(self.subjectCode)
+        self.cpmkGrades= []
+        self.nim = nim
+        self.nip = nip
+        self.year = year
+        self.grade = 0
+
+        self.setCpmkGrades()
+        self.setGrade()
+
+    def setCpmkGrades(self):
+        for c in self.subject.cpmk:
+            cpmkGrade = CpmkGrade(self.subject.code, c.code, self.nim, self.nip, self.year)
+            self.cpmkGrades.append(cpmkGrade)
+
+    def setGrade(self):
+        finalExamGrade = FinalExamGrade(self.subjectCode, self.nim, self.nip, self.year)
+        finalExamGradePercent = 100
+        
+        for g in self.cpmkGrades:
+            self.grade += g.grade*g.subjectGradePercent/100
+            finalExamGradePercent -= g.subjectGradePercent
+        
+        self.grade += finalExamGrade.grade*finalExamGradePercent/100
+
+class Enroll:
+    def __init__(self, nim, year):
+        self.nim = nim
+        self.year = year
+        self.enrolled = []
+        
+        self.setEnrolled()
+
+    def setEnrolled(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM enroll WHERE NIM = "%s" AND YEAR(date) = %s' % (self.nim, self.year))
+        result = cursor.fetchall()
+        conn.close()
+
+        for entry in result:
+            subject = Subject(entry[1])
+            self.enrolled.append(subject)
+
+class Cpl:
+    def __init__(self, code):
+        self.code = code
+        self.name = ''
+        self.subjectCode = ''
+
+        self.setName()
+        self.setSubjectCode()
+
+    def setName(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT cplDesc FROM cpl WHERE cplCode = "%s"' % (self.code,))
+        self.name = cursor.fetchone()[0]
+        conn.close()
+    
+    def setSubjectCode(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT subjectCode FROM cpl WHERE cplCode = "%s"' % (self.code,))
+        self.subjectCode = cursor.fetchone()[0]
+        conn.close()
